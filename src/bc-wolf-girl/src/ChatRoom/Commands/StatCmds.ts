fe5c1973d256e7152ba89e98b7c343aac4993e1e
dynilath <dynilath@qq.com>
2024-02-24 05:16:39 +0800

    fix: redundant words in stat cmd

--- src/bc-wolf-girl/src/ChatRoom/Commands/StatCmds.ts
+++ src/bc-wolf-girl/src/ChatRoom/Commands/StatCmds.ts
@@ -1,7 +1,7 @@
 import { CommonWork, DelayWork } from "../../Control/CommonWork";
 import { ParseMessage } from "../../Control/Message";
 import { MessageWork } from "../../Control/MessageWork";
-import { GetWolfGrilName } from "../../Control/WolfGirlCtrl";
+import { GetWolfGirlName } from "../../Control/WolfGirlCtrl";
 import { TimedWorkState, TimedWorker } from "../../Control/Worker";
 import { DataManager } from "../../Data";
 import { CommandTemplate } from "../ICmds";
@@ -14,7 +14,7 @@ export const StatCmds: CommandTemplate[] = [
         prerequisite: ModOrSelfPrerequisites,
         run(player, sender, content) {
             const { orgasm, resist, ruined } = DataManager.arousal;
-            const arousal_report = GetWolfGrilName(player) + "的高潮统计如下：\n" +
+            const arousal_report = GetWolfGirlName(player) + "的高潮统计如下：\n" +
                 `  高潮次数：${orgasm}\n` +
                 `  忍耐次数：${resist}\n` +
                 `  毁灭次数：${ruined}\n` +
@@ -45,7 +45,7 @@ export const StatCmds: CommandTemplate[] = [
                 return new Date(time).toLocaleString();
             }
 
-            const task_report = GetWolfGrilName(player) + "的训练统计如下：\n" +
+            const task_report = GetWolfGirlName(player) + "的训练统计如下：\n" +
                 `  训练辅助系统总运行时间：${day_hour_minute_second(script_run_time)}\n` +
                 `  训练模块有效工作时间：${day_hour_minute_second(wolfgirl_time)}\n` +
                 `  任务完成次数：${finished}\n` +
@@ -68,18 +68,18 @@ export const StatCmds: CommandTemplate[] = [
             TimedWorker.global.push({
                 description: "ScanBody", works: [
                     new MessageWork({ mode: "action", msg: "指令收到，正在扫描身体参数" }),
-                    new MessageWork({ mode: "action", msg: `狼女{player_wg}的项圈上涌出了数股金属银色的小小水流，正沿着狼女{player_wg}的身体缓缓流向身体各处，短暂的覆盖住了狼女{player_wg}的身体，如同银色的乳胶衣一般覆盖了全身，随着一阵刺痒过后，这件奇特的乳胶衣又化回了银色的水流，原路返回项圈之中` }),
+                    new MessageWork({ mode: "action", msg: `{player_wg}的项圈上涌出了数股金属银色的小小水流，正沿着{player_wg}的身体缓缓流向身体各处，短暂的覆盖住了{player_wg}的身体，如同银色的乳胶衣一般覆盖了全身，随着一阵刺痒过后，这件奇特的乳胶衣又化回了银色的水流，原路返回项圈之中` }),
                     new DelayWork(5000),
                     new CommonWork((player) => {
                         if (player.ArousalSettings === undefined || player.ArousalSettings.Active === "Inactive") {
-                            ParseMessage({ mode: "action", msg: "扫描数据分析完毕，狼女{player_wg}的性奋中枢处于关闭状态，没有收集到任何身体数据。" }, { player });
+                            ParseMessage({ mode: "action", msg: "扫描数据分析完毕，{player_wg}的性奋中枢处于关闭状态，没有收集到任何身体数据。" }, { player });
                             return TimedWorkState.interrupted;
                         }
                         const { Zone } = player.ArousalSettings;
                         const value3Zones = Zone.filter(z => z.Factor === 3).map(z => AssetGroupGet(player.AssetFamily, z.Name)?.Description).filter(z => !!z).join("、");
                         const value4Zones = Zone.filter(z => z.Factor === 4).map(z => AssetGroupGet(player.AssetFamily, z.Name)?.Description).filter(z => !!z).join("、");
                         const orgasmZones = Zone.filter(z => z.Orgasm).map(z => AssetGroupGet(player.AssetFamily, z.Name)?.Description).filter(z => !!z).join("、");
-                        ParseMessage({ mode: "action", msg: `扫描数据已分析完毕，狼女{player_wg}身体较为敏感的部位为 {value3Zones} ，狼女{player_wg}身体十分敏感的部位为 {value4Zones} ，其中，能够使狼女{player_wg}高潮的部位为 {orgasmZones} ，扫描数据总结完成` },
+                        ParseMessage({ mode: "action", msg: `扫描数据已分析完毕，{player_wg}身体较为敏感的部位为 {value3Zones} ，{player_wg}身体十分敏感的部位为 {value4Zones} ，其中，能够使{player_wg}高潮的部位为 {orgasmZones} ，扫描数据总结完成` },
                             { player }, { value3Zones, value4Zones, orgasmZones });
                     })
                 ]
